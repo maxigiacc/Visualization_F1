e94abf694df5ab0b272ebd9395bfa4dc591fc4f8
Massimo Giaccone <giacconemassimogmail.com>
2025-11-08 18:54:57 +0100

    Update: added linechart tries regarding factors and km travelled

--- simple-map/src/App.tsx
+++ simple-map/src/App.tsx
@@ -3,11 +3,16 @@ import Map from "./Map";
 import "./App.css";
 import InteractiveMap from "./InteractiveMap";
 
+import ApexCsvRealtimeChart from './components/ApexCsvRealtimeChart.jsx';
+import TravelKmPerYearChart from './components/TravelKmPerYearChart.jsx';
+
 function App() {
     return (
         <>
             <h1>F1 Visualisation</h1>
             <Map />
+            <ApexCsvRealtimeChart csvUrl="/emission_factors_2000_2025.csv" />.
+            <TravelKmPerYearChart />
         </>
     );
 }
